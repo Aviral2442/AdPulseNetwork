a6310c9321cc5c43074e803a86f48ebab576df8e
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-06-28 22:37:32 +0530

    Top Header is completed

--- frontend/src/app/(main)/navbar/topHeader.jsx
+++ frontend/src/app/(main)/navbar/topHeader.jsx
@@ -1,12 +1,38 @@
 'use client'
-import { Container } from '@mantine/core'
+import { Anchor, Container, Group, Text } from '@mantine/core'
 import classes from './navbar.module.css'
 import React from 'react'
+import { IconBrandFacebook, IconBrandInstagram, IconBrandLinkedin, IconBrandTiktok, IconBrandX, IconBrandYoutube } from '@tabler/icons-react'
 
 const TopHeader = () => {
     return (
-        <Container size="xl" bg={'#F0F2F4'} mt={8} className={classes.TopHeader} >
-            Container with custom size
+        <Container size="xl" p={2} c="white" bg={'#333333'} mt={8} className={classes.TopHeader} >
+            <Group justify="space-between" px={40} >
+                <Group justify="space-between" gap={50} >
+                    <Text size='sm' className={classes.TopHeaderDetails} >Call us: +91 2497534975</Text>
+                    <Text size='sm' className={classes.TopHeaderDetails} >Email: contact@adpulsenetworl.com</Text>
+                </Group>
+                <div >
+                    <Anchor href='#' c="#FFFFFF" size="sm">
+                    <IconBrandFacebook size={25} className={classes.socialIcons} />
+                    </Anchor>
+                    <Anchor href='#' c="#FFFFFF" size="sm">
+                    <IconBrandInstagram size={25} className={classes.socialIcons} />
+                    </Anchor>
+                    <Anchor href='#' c="#FFFFFF" size="sm">
+                    <IconBrandLinkedin size={25} className={classes.socialIcons} />
+                    </Anchor>
+                    <Anchor href='#' c="#FFFFFF" size="sm">
+                    <IconBrandX size={25} className={classes.socialIcons} />
+                    </Anchor>
+                    <Anchor href='#' c="#FFFFFF" size="sm">
+                    <IconBrandTiktok size={25} className={classes.socialIcons} />
+                    </Anchor>
+                    <Anchor href='#' c="#FFFFFF" size="sm">
+                    <IconBrandYoutube size={25} className={classes.socialIcons} />
+                    </Anchor>
+                </div>
+            </Group>
         </Container>
     )
 }
