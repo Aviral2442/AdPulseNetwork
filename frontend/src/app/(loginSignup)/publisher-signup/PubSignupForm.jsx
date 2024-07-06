500ba10cd7f704978ee23cd3a918f8162a192967
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-07-06 09:27:26 +0530

    creating login form

---
+++ frontend/src/app/(loginSignup)/publisher-signup/PubSignupForm.jsx
@@ -0,0 +1,145 @@
+'use client'
+import React from 'react';
+import { useState } from "react";
+import { Stepper, Button, Group, TextInput, PasswordInput, Code} from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+const PubSignupForm = () => {
+
+  const [active, setActive] = useState(0)
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      username: "",
+      password: "",
+      name: "",
+      email: "",
+      website: "",
+      github: ""
+    },
+
+    validate: values => {
+      if (active === 0) {
+        return {
+          username:
+            values.username.trim().length < 6
+              ? "Username must include at least 6 characters"
+              : null,
+          password:
+            values.password.length < 6
+              ? "Password must include at least 6 characters"
+              : null
+        }
+      }
+
+      if (active === 1) {
+        return {
+          name:
+            values.name.trim().length < 2
+              ? "Name must include at least 2 characters"
+              : null,
+          email: /^\S+@\S+$/.test(values.email) ? null : "Invalid email"
+        }
+      }
+
+      return {}
+    }
+  })
+
+  const nextStep = () =>
+    setActive(current => {
+      if (form.validate().hasErrors) {
+        return current
+      }
+      return current < 3 ? current + 1 : current
+    })
+
+  const prevStep = () =>
+    setActive(current => (current > 0 ? current - 1 : current))
+
+  return (
+    <>
+    <Stepper active={active}>
+      <Stepper.Step label="First step" description="General Info.">
+
+      <TextInput
+        label="Name"
+        placeholder="Name"
+      />
+      <TextInput
+        mt="sm"
+        label="Email"
+        placeholder="Email"
+      />
+
+        {/* <TextInput
+          label="Username"
+          placeholder="Username"
+          key={form.key("username")}
+          {...form.getInputProps("username")}
+        />
+        <PasswordInput
+          mt="md"
+          label="Password"
+          placeholder="Password"
+          key={form.key("password")}
+          {...form.getInputProps("password")}
+        /> */}
+
+      </Stepper.Step>
+
+      <Stepper.Step label="Second step" description="Personal Details">
+
+        <TextInput
+          label="Name"
+          placeholder="Name"
+          key={form.key("name")}
+          {...form.getInputProps("name")}
+        />
+        <TextInput
+          mt="md"
+          label="Email"
+          placeholder="Email"
+          key={form.key("email")}
+          {...form.getInputProps("email")}
+        />
+
+      </Stepper.Step>
+
+      <Stepper.Step label="Final step" description="Password">
+        <TextInput
+          label="Website"
+          placeholder="Website"
+          key={form.key("website")}
+          {...form.getInputProps("website")}
+        />
+        <TextInput
+          mt="md"
+          label="GitHub"
+          placeholder="GitHub"
+          key={form.key("github")}
+          {...form.getInputProps("github")}
+        />
+      </Stepper.Step>
+      <Stepper.Completed>
+        Completed! Form values:
+        <Code block mt="xl">
+          {JSON.stringify(form.getValues(), null, 2)}
+        </Code>
+      </Stepper.Completed>
+    </Stepper>
+
+    <Group justify="flex-end" mt="xl">
+      {active !== 0 && (
+        <Button variant="default" onClick={prevStep}>
+          Back
+        </Button>
+      )}
+      {active !== 3 && <Button onClick={nextStep}>Next step</Button>}
+    </Group>
+  </>
+  )
+}
+
+export default PubSignupForm
--- frontend/src/app/(loginSignup)/publisher-signup/page.jsx
+++ frontend/src/app/(loginSignup)/publisher-signup/page.jsx
@@ -1,20 +1,37 @@
 'use client'
-import { Container, Flex, Grid, Paper } from '@mantine/core'
+import { Container, Flex, Grid, Paper, Text } from '@mantine/core'
 import React from 'react'
+import classes from './publisherSignup.module.css'
+import PubSignupForm from './PubSignupForm'
 
 const PublisherSignup = () => {
   return (
-    <Container fluid bg={"gray"}>
+    <Container fluid bg="#F0F2F4" h={"100vh"}>
       <Paper>
         <Flex
-          mih={50}
+          mih={"100vh"}
           justify="center"
           align="center"
-          bg={"gray"}
+          bg={"#F0F2F4"}
         >
-          <Grid bg={"white"}  >
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>1</Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>2</Grid.Col>
+          <Grid bg={"#FFFFFF"} className={classes.grid} >
+            <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+              <div className={classes.rightSection} >
+                <Text  py={15}  align="center" weight={700} size="xl">Publisher Signup</Text>
+                <Text  py={15} >Register Now to Unlock A Range of Advertising Benefits!</Text>
+                <Text  py={15} >7Search PPC might be your gateway to success and a one-stop solution for all your business advertising needs.
+                  Whether you are a small enterprise or a big business giant, we have the potential to boost your revenue
+                  tremendously.</Text>
+                <Text py={15} >Begin your advertising journey with just $100 at 7Search PPC by filling in the required details and
+                  submitting today!</Text>
+                <Text py={15} >Please ensure that your website complies with all our advertising Terms and Policies to promote your business effectively.</Text>
+              </div>
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+              <div className={classes.leftSection} >
+                  <PubSignupForm />
+              </div>
+            </Grid.Col>
           </Grid>
         </Flex>
       </Paper>
