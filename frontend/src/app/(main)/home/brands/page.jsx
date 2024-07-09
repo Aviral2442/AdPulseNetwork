6aef89e0547c899ddbe89d95029519691ca352d5
sachin2745 <sachin.official648@gmail.com>
2024-07-09 20:52:56 +0530

    brand is designed and responsive

--- frontend/src/app/(main)/home/brands/page.jsx
+++ frontend/src/app/(main)/home/brands/page.jsx
@@ -1,49 +1,57 @@
 "use client";
-import { Container, Grid, Image, Text, Title } from '@mantine/core';
+import React from 'react';
+import { Image, SimpleGrid, Text, Title } from '@mantine/core';
 import { Manrope, Sora } from 'next/font/google';
-import React from 'react'
-import classes from './brands.module.css';
+import '@mantine/carousel/styles.css';
+import { Carousel } from '@mantine/carousel';
 import clsx from 'clsx';
+import classes from './brands.module.css';
 
 const font = Sora({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] });
 const fonts = Manrope({ subsets: ['latin'], weight: ['200', '300', '400', '500'] });
 
 const Brands = () => {
     return (
-
         <div className={classes.wrapper}>
-
             <Text className={fonts.className}>/partners/</Text>
-            <Title fw={400} order={1} mb={19} className={clsx(classes.Title,font.className)}>Big Brands & Exclusive Advertisers</Title>
+            <Title fw={400} order={1} mb={19} className={clsx(classes.Title, font.className)}>Big Brands & Exclusive Advertisers</Title>
+            <SimpleGrid
+                className={classes.grid}
+                cols={{ base: 1, sm: 2, lg: 5 }}
+                spacing={{ base: 10, sm: 'xl' }}
+                verticalSpacing={{ base: 'md', sm: 'xl' }}
+            >
+                {['01.png', '02.png', '03.png', '04.png', '05.png'].map((src, index) => (
+                    <div key={index} className={classes.imageWrapper}>
+                        <Image src={src} alt={`Brand ${index + 1}`} fit="contain" />
+                    </div>
+                ))}
+            </SimpleGrid>
 
-            <Grid grow gutter="xl">
-                <Grid.Col span={1.5} >
-                    <Image src="01.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="02.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="03.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="04.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="05.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="05.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="05.png" alt="" />
-                </Grid.Col>
-                <Grid.Col span={1.5}>
-                    <Image src="05.png" alt="" />
-                </Grid.Col>
-            </Grid>
+            <Carousel
+
+                withIndicators
+                className={classes.carousel}
+                classNames={classes}
+                height={150}
+                withControls={false}                
+                loop
+                align="center"
+            >
+                {['01.png', '02.png', '03.png', '04.png', '05.png'].map((src, index) => (
+                    <Carousel.Slide key={index}>
+                        <div className={classes.imageWrapper1}>
+                            <Image src={src} alt={`Brand ${index + 1}`} fit="contain" className={classes.image1} />
+                        </div>
+                       
+
+                    </Carousel.Slide>
+                ))}
+
+            </Carousel>
+            
         </div>
-    )
+    );
 }
 
-export default Brands
+export default Brands;
