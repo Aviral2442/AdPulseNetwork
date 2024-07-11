9b199307e72a5fc0738f4cee2361ac3512e18240
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-07-12 00:45:51 +0530

    know more section is created

--- frontend/src/app/(main)/home/knowmore/page.jsx
+++ frontend/src/app/(main)/home/knowmore/page.jsx
@@ -5,30 +5,30 @@ import React from 'react'
 const KnowMore = () => {
   return (
     <Container fluid p={10} >
-        <div>
-          <SimpleGrid
-            cols={{ base: 1, sm: 2, lg: 3 }}
-            p={40}
-          >
-            <div>
-              <Image
-                radius="md"
-                src="image1.png"
-              />
-            </div>
-            <div>
-              <Image
-                radius="md"
-                src="image2.png"
-                />
-            </div>
-            <div>
-              <Image
-                radius="md"
-                src="image8.png"
-                />
-            </div>
-            {/* <div>
+      <div>
+        <SimpleGrid
+          cols={{ base: 1, sm: 2, lg: 3 }}
+          p={40}
+        >
+          <div>
+            <Image
+              radius="md"
+              src="image1.png"
+            />
+          </div>
+          <div>
+            <Image
+              radius="md"
+              src="image2.png"
+            />
+          </div>
+          <div>
+            <Image
+              radius="md"
+              src="image8.png"
+            />
+          </div>
+          {/* <div>
               <Image
                 p={5}
                 radius="md"
@@ -46,8 +46,8 @@ const KnowMore = () => {
                 src="image6.png"
               />
             </div> */}
-          </SimpleGrid>
-        </div>
+        </SimpleGrid>
+      </div>
     </Container>
   )
 }
