6aba91d91af6bddce32158e0ea79bc4f82c15da3
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-07-11 12:49:30 +0530

    top header is all set

--- frontend/src/app/(main)/navbar/topHeader.jsx
+++ frontend/src/app/(main)/navbar/topHeader.jsx
@@ -2,34 +2,34 @@
 import { Anchor, Container, Group, Text } from '@mantine/core'
 import classes from './navbar.module.css'
 import React from 'react'
-import { IconBrandFacebook, IconBrandInstagram, IconBrandLinkedin, IconBrandTiktok, IconBrandX, IconBrandYoutube } from '@tabler/icons-react'
+import { FaFacebook, FaInstagramSquare, FaLinkedin } from 'react-icons/fa'
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { AiFillTikTok } from "react-icons/ai";
+import { IoLogoYoutube } from 'react-icons/io5'
 
 const TopHeader = () => {
     return (
         <Container size="xl" p={2} c="white" bg={'#333333'} mt={8} className={classes.TopHeader} >
-            <Group justify="space-between" px={40} >
-                <Group justify="space-between" gap={50} >
-                    <Text size='sm' className={classes.TopHeaderDetails} >Call us: +91 2497534975</Text>
-                    <Text size='sm' className={classes.TopHeaderDetails} >Email: contact@adpulsenetworl.com</Text>
-                </Group>
+            <Group justify="space-between" px={15} >
+                <Text  className={classes.TopHeaderDetails} >Email: <span className={classes.socialIcons}> contact@adpulsenetwork.com </span> </Text>
                 <div >
-                    <Anchor href='#' c="#FFFFFF" size="sm">
-                    <IconBrandFacebook size={25} className={classes.socialIcons} />
+                    <Anchor href='https://www.facebook.com/adpulsenetwork' c="#FFFFFF" size="sm">
+                        <FaFacebook size={26} className={classes.socialIcons} />
                     </Anchor>
-                    <Anchor href='#' c="#FFFFFF" size="sm">
-                    <IconBrandInstagram size={25} className={classes.socialIcons} />
+                    <Anchor href='https://www.instagram.com/adpulsenetwork/' c="#FFFFFF" size="sm">
+                        <FaInstagramSquare size={25} className={classes.socialIcons} />
                     </Anchor>
-                    <Anchor href='#' c="#FFFFFF" size="sm">
-                    <IconBrandLinkedin size={25} className={classes.socialIcons} />
+                    <Anchor href='https://www.linkedin.com/company/ad-pulse-network/' c="#FFFFFF" size="sm">
+                        <FaLinkedin size={25} className={classes.socialIcons} />
                     </Anchor>
-                    <Anchor href='#' c="#FFFFFF" size="sm">
-                    <IconBrandX size={25} className={classes.socialIcons} />
+                    <Anchor href='https://x.com/Adpulsenetwork' c="#FFFFFF" size="sm">
+                        <FaSquareXTwitter size={25} className={classes.socialIcons} />
                     </Anchor>
-                    <Anchor href='#' c="#FFFFFF" size="sm">
-                    <IconBrandTiktok size={25} className={classes.socialIcons} />
+                    <Anchor href='https://www.tiktok.com/@adpulsenetwork' c="#FFFFFF" size="sm">
+                        <AiFillTikTok size={27} className={classes.socialIcons} />
                     </Anchor>
-                    <Anchor href='#' c="#FFFFFF" size="sm">
-                    <IconBrandYoutube size={25} className={classes.socialIcons} />
+                    <Anchor href='https://www.youtube.com/@adpulssenetwork' c="#FFFFFF" size="sm">
+                        <IoLogoYoutube size={27} className={classes.socialIcons} />
                     </Anchor>
                 </div>
             </Group>
