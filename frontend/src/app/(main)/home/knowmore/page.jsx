90d49e0d7fa1b03f9509429bab07b2559f65f958
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-07-30 20:13:10 +0530

    all changes done

--- frontend/src/app/(main)/home/knowmore/page.jsx
+++ frontend/src/app/(main)/home/knowmore/page.jsx
@@ -4,35 +4,46 @@ import React from 'react'
 
 const KnowMore = () => {
   return (
-    <Container fluid p={10} >
-      <div>
-        <SimpleGrid
-          cols={{ base: 1, sm: 3, lg: 3 }}
-          p={40}
-        >
-          <div>
-            <Image
-              radius="md"
-              src="image1.png"
-              alt='Image 1'
-            />
-          </div>
-          <div>
-            <Image
-              radius="md"
-              src="image2.png"
-              alt='Image 2'
-            />
-          </div>
-          <div>
-            <Image
-              radius="md"
-              src="image8.png"
-              alt='Image 3'
-            />
-          </div>
-        </SimpleGrid>
-      </div>
+    <Container
+      fluid
+      sx={(theme) => ({
+        padding: 10,
+        [theme.fn.smallerThan('sm')]: {
+          padding: 2,
+        },
+      })}
+    >
+      <SimpleGrid
+        cols={3}
+        breakpoints={[
+          { maxWidth: 'sm', cols: 1 },
+          { maxWidth: 'md', cols: 2 },
+          { maxWidth: 'lg', cols: 3 },
+        ]}
+        p={40}
+      >
+        <div>
+          <Image
+            radius="md"
+            src="image1.png"
+            alt='Image 1'
+          />
+        </div>
+        <div>
+          <Image
+            radius="md"
+            src="image2.png"
+            alt='Image 2'
+          />
+        </div>
+        <div>
+          <Image
+            radius="md"
+            src="image8.png"
+            alt='Image 3'
+          />
+        </div>
+      </SimpleGrid>
     </Container>
   )
 }
