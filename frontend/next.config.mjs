1c9fa0315f3a3997689d9aaa3fb243b2ca2d17ec
Aviral2442 <aviralsrivastava24y15@gmail.com>
2024-09-01 09:07:00 +0530

    some changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,6 @@
-// next.config.js
+// next.config.mjs
 const nextConfig = {
-    // your config options here
-  };
-  
-  module.exports = nextConfig;
-  
+  // your config options here
+};
+
+export default nextConfig;
